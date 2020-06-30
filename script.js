8d29aeaa312f4540b22a3371e1edb924adb536a5
z19zc032 <66655243+z19zc032@users.noreply.github.com>
2020-06-30 11:33:29 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// buttonSet1
+$( function() {
+
+  $( '#buttonHeader' ).click( function() {
+    $( 'h1' ).text( 'Hello jQuery!' );
+  });
+
+  $( '#buttonArticle' ).click( function() {
+    $( 'article' ).css({
+      'color': 'white',
+      'background-color': 'red'
+    });
+  });
+
+  $( '#buttonFooter' ).click( function() {
+    $( 'footer' ).html('<p><a href="https://www.kyusan-u.ac.jp">Kyushu Sangyo University</a></p>' );
+    $( 'footer a' ).css( 'color','red' );
+  });
+
+});
+
+// buttonSet2
+$( function() {
+
+  $( '#buttonNormal' ).click( function() {
+    $('main').css( 'flex-direction','row' );
+  });
+
+  $( '#buttonReverse' ).click( function() {
+    $('main').css( 'flex-direction','row-reverse' );
+  });
+
+  $( '#buttonVertical' ).click( function() {
+    $('main').css( 'flex-direction','column' );
+  });
+
+
+
+});
